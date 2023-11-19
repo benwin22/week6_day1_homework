80ccb6f15e6f5a1361fc5e1635934ef2f0460e45
benwin22 <145385526+benwin22@users.noreply.github.com>
2023-11-19 10:54:12 -0600

    final commit fingers crossed

--- benj_shop/__init__.py
+++ benj_shop/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask
 from flask_migrate import Migrate
-
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
 #internal imports
 from .blueprints.site.routes import site
 from .blueprints.auth.routes import auth
+from .blueprints.api.routes import api
 from config import Config
 from .models import login_manager, db
+from .helpers import JSONEncoder
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+jwt = JWTManager(app)
 
 #wrap our app in login_manager so we can use it wherever in our app
 login_manager.init_app(app)
@@ -20,7 +24,11 @@ login_manager.login_message_category = 'warning'
 
 app.register_blueprint(site)
 app.register_blueprint(auth)
+app.register_blueprint(api)
+
 
 #instantiating our database & wrapping our app
 db.init_app(app)
 migrate = Migrate(app, db)
+app.json_encoder = JSONEncoder
+cors = CORS(app)
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 import os
 from dotenv import load_dotenv #allows us to load our environment variables(to fun application)
 
@@ -24,3 +25,5 @@ class Config():
     SECRET_KEY = os.environ.get('SECRET KEY') or 'secret key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=365)
